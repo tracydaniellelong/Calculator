baaded1f974b6ba90f28737ccbb5fdb1f2f6e7e7
tracydaniellelong <tracydaniellelong@gmail.com>
2018-11-14 14:47:45 -0500

    finished calculations

--- calculator.rb
+++ calculator.rb
@@ -12,20 +12,31 @@ end
 
 get '/numbers' do
 	user_name = params[:user_name]
-	operator = params[:operator]
-	num1 = params[:num1]
-	num2 = params[:num2]
-	erb :numbers, locals: {user_name: user_name, operator: operator, num1: num1, num2: num2}
+	operator
+	erb :numbers, locals: {user_name: user_name}
 end
 
 post '/numbers' do
-	user_name = params[:user_name]
 	operator = params[:operator]
-	num1 = params[:num1].to_i
-	num2 = params[:num2].to_i
+	num1 = params[:num1]
+	num2 = params[:num2]
+	redirect '/results?num1=' + num1 + '&num2=' + num2 + '&operator=' + operator  
+end
+
+get '/results' do
+	operator = params[:operator]
+	num1 = params[:num1].to_f
+	num2 = params[:num2].to_f
 	if operator == "add"
 		total = add(num1, num2)
-	elsif operator == subtract
+	elsif operator == "subtract"
 		total = subtract(num1, num2)
+	elsif operator == "multiply"
+		total = multiply(num1, num2)
+	else
+		if num1 >= 1 && num2 > 1
+			total = divide(num1, num2)
+		end
 	end
+	erb :results, locals: {operator: operator, num1: num1, num2: num2, total: total}
 end
